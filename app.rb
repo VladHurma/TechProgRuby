8aae496327c63da64fe8ee95dfac779d04a96e06
VladHurma <vladik_shyshko@mail.ru>
2018-12-22 09:38:41 +0300

    Ver 1.0.1
    Restyle code and add table destroy with session destroy

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-#encoding: utf-8
+# frozen_string_literal: true
 
 require 'rubygems'
 require 'sinatra'
@@ -8,39 +8,39 @@ require './lib/validator'
 require './servises/servises'
 
 def err_checker(page, params, block)
-	@error = Validator.err_check page, params
-		if @error != ''
-			return erb page
-		else
-			block.call params
-		end
+  @error = Validator.err_check page, params
+  if @error != ''
+    return erb page
+  else
+    block.call params
+  end
 end
 
 configure do
-	enable :sessions
-	Servise.new.initialize_tables
+  enable :sessions
+  Servise.new.initialize_tables
 end
 
 helpers do
-	def username
-		session[:identity] ? session[:identity] : 'Hello, who are you?'
-	end
+  def username
+    session[:identity] || 'Hello, who are you?'
+  end
 end
 
 before '*' do
-	@servise = Servise.new
+  @servise = Servise.new
 end
 
 before '/secure/*' do
-	unless session[:identity]
-		session[:previous_url] = request.path
-		@error = "Ты должен назвать себя, чтобы получить доступ к #{request.path}"
-		halt erb(:log_in)
-	end
+  unless session[:identity]
+    session[:previous_url] = request.path
+    @error = "Ты должен назвать себя, чтобы получить доступ к #{request.path}"
+    halt erb(:log_in)
+  end
 end
 
 get '/' do
-	redirect to '/log_in'
+  redirect to '/log_in'
 end
 
 get '/log_in' do
@@ -48,90 +48,87 @@ get '/log_in' do
 end
 
 post '/log_in' do
-	block = lambda do |params|
-	  session[:identity] = params[:username]
-	  where_user_came_from = session[:previous_url] || '/secure/table_of_family'
-	
-	  redirect to '/secure/table_of_family'
-	end
+  block = lambda do |params|
+    session[:identity] = params[:username]
+    where_user_came_from = session[:previous_url] || '/secure/table_of_family'
 
-	err_checker :log_in, params, block
+    redirect to '/secure/table_of_family'
+  end
+
+  err_checker :log_in, params, block
 end
 
 get '/logout' do
   session.delete(:identity)
   erb "<div class='alert alert-message'>Logged out</div>"
+  @servise.destroy
   redirect to '/log_in'
 end
 
-
 get '/secure/table_of_family' do
-	erb :table_of_family
+  erb :table_of_family
 end
 
 post '/secure/table_of_family' do
-	@login = params[:login]
-	@password = params[:password]
+  @login = params[:login]
+  @password = params[:password]
 
-	if @login == "admin" && @password == "secret"
-		@table_content = {
-			:earn_content => [],
-			:fact_pay_content => [],
-			:planned_pay_content => [],
-			:sum_of_earnings => 0,
-			:sum_of_fact_payings => 0,
-			:sum_of_planned_payings => 0,
-			:result_money => 0
-		}
+  if @login == 'admin' && @password == 'secret'
+    @table_content = {
+      earn_content: [],
+      fact_pay_content: [],
+      planned_pay_content: [],
+      sum_of_earnings: 0,
+      sum_of_fact_payings: 0,
+      sum_of_planned_payings: 0,
+      result_money: 0
+    }
 
-		@servise.show @table_content
+    @servise.show @table_content
 
-		@table_content[:result_money] = @table_content[:sum_of_earnings] - @table_content[:sum_of_fact_payings]
-		
-		erb :show_table_of_family
-    else
-		redirect to "/secure/table_of_family"
-	end
-			
+    @table_content[:result_money] = @table_content[:sum_of_earnings] - @table_content[:sum_of_fact_payings]
+
+    erb :show_table_of_family
+  else
+    redirect to '/secure/table_of_family'
+  end
 end
 
-
 get '/secure/earn' do
-	erb :earn
+  erb :earn
 end
 
 post '/secure/earn' do
-	block = lambda do |params|
-		@servise.create_earning params
-		erb :earn
-	end
+  block = lambda do |params|
+    @servise.create_earning params
+    erb :earn
+  end
 
-	err_checker :earn, params, block
+  err_checker :earn, params, block
 end
 
-
 get '/secure/fact_payment' do
-	erb :fact_payment
+  erb :fact_payment
 end
 
 post '/secure/fact_payment' do
-	block = lambda do |params|
-		@servise.create_fact_paying params
-		erb :fact_payment
-	end
+  block = lambda do |params|
+    @servise.create_fact_paying params
+    erb :fact_payment
+  end
 
-	err_checker :fact_payment, params, block
+  err_checker :fact_payment, params, block
 end
 
 get '/secure/planned_payment' do
-	erb :planned_payment
+  erb :planned_payment
 end
 
-post '/secure/planned_payment' do	
-	block = lambda do |params|
-		@servise.create_planned_paying params
-		erb :planned_payment
-	end
-	
-	err_checker :planned_payment, params, block
+post '/secure/planned_payment' do
+  block = lambda do |params|
+    @servise.create_planned_paying params
+    erb :planned_payment
+  end
+
+  err_checker :planned_payment, params, block
 end
--- servises/servises.rb
+++ servises/servises.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 class Servise
+  def initialize
+    @db = SQLite3::Database.new 'db/family_money.db'
+  end
 
-	def initialize
-		@db = SQLite3::Database.new 'db/family_money.db'
-	end
-
-	def initialize_tables
-		@db.execute 'CREATE TABLE IF NOT EXISTS
+  def initialize_tables
+    @db.execute 'CREATE TABLE IF NOT EXISTS
 			"Earnings"
 			(
 				"id" INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,7 +17,7 @@ class Servise
 				"placeholder_of_earning" TEXT,
 				"amount" INTEGER
 			)'
-		@db.execute 'CREATE TABLE IF NOT EXISTS
+    @db.execute 'CREATE TABLE IF NOT EXISTS
 			"Fact_payings"
 			(
 				"id" INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -25,51 +26,57 @@ class Servise
 				"username" TEXT,
 				"amount" INTEGER
 			)'
-		@db.execute 'CREATE TABLE IF NOT EXISTS
+    @db.execute 'CREATE TABLE IF NOT EXISTS
 			"Planned_payings"
 			(
 				"id" INTEGER PRIMARY KEY AUTOINCREMENT,
 				"sourse" TEXT,
 				"amount" INTEGER
 			)'
-	end
+  end
 
-	def create_earning(params)
-		@db.execute 'INSERT INTO
+  def create_earning(params)
+    @db.execute 'INSERT INTO
 			Earnings (date, sourse, username, family_role, placeholder_of_earning,amount)
-			values (?, ?, ?, ?, ?, ?)', [params[:date], params[:sourse], params[:username], params[:family_role], params[:placeholder_of_earning], params[:amount]]		
-	end
+			values (?, ?, ?, ?, ?, ?)', [params[:date], params[:sourse], params[:username], params[:family_role], params[:placeholder_of_earning], params[:amount]]
+  end
 
-	def create_fact_paying(params)
-		@db.execute 'INSERT INTO
+  def create_fact_paying(params)
+    @db.execute 'INSERT INTO
 			Fact_payings (date, sourse, username, amount)
-			values (?, ?, ?, ?)', [params[:date], params[:sourse], params[:username],params[:amount]]
-	end
+			values (?, ?, ?, ?)', [params[:date], params[:sourse], params[:username], params[:amount]]
+  end
 
-	def create_planned_paying(params)
-		@db.execute 'INSERT INTO
+  def create_planned_paying(params)
+    @db.execute 'INSERT INTO
 			Planned_payings (sourse, amount)
 			values (?, ?)', [params[:sourse], params[:amount]]
-	end
+  end
 
-	def show(table_content)
-		@db.execute 'SELECT * FROM Earnings' do |row|
-			table_content[:earn_content] << row
-		end
-		@db.execute 'SELECT * FROM Fact_payings' do |row|
-			table_content[:fact_pay_content] << row
-		end
-		@db.execute 'SELECT * FROM Planned_payings' do |row|
-			table_content[:planned_pay_content] << row
-		end
-		@db.execute 'SELECT amount FROM Earnings' do |amount|
-			table_content[:sum_of_earnings] += amount[0]
-		end
-		@db.execute 'SELECT amount FROM Fact_payings' do |amount|
-			table_content[:sum_of_fact_payings] += amount[0]
-		end
-		@db.execute 'SELECT amount FROM Planned_payings' do |amount|
-			table_content[:sum_of_planned_payings] += amount[0]
-		end
-	end
+  def show(table_content)
+    @db.execute 'SELECT * FROM Earnings' do |row|
+      table_content[:earn_content] << row
+    end
+    @db.execute 'SELECT * FROM Fact_payings' do |row|
+      table_content[:fact_pay_content] << row
+    end
+    @db.execute 'SELECT * FROM Planned_payings' do |row|
+      table_content[:planned_pay_content] << row
+    end
+    @db.execute 'SELECT amount FROM Earnings' do |amount|
+      table_content[:sum_of_earnings] += amount[0]
+    end
+    @db.execute 'SELECT amount FROM Fact_payings' do |amount|
+      table_content[:sum_of_fact_payings] += amount[0]
+    end
+    @db.execute 'SELECT amount FROM Planned_payings' do |amount|
+      table_content[:sum_of_planned_payings] += amount[0]
+    end
+  end
+
+  def destroy
+    @db.execute 'DROP TABLE Earnings'
+    @db.execute 'DROP TABLE Fact_payings'
+    @db.execute 'DROP TABLE Planned_payings'
+  end
 end
